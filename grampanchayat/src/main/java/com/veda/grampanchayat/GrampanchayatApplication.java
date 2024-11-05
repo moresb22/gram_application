ea585fd9ae8b644cde4d6bdbf1f47135ee8d506d
Shubham More <moressb96@gmail.com>
2024-11-05 12:54:42 +0530

    All Project Files updates

---
+++ grampanchayat/src/main/java/com/veda/grampanchayat/GrampanchayatApplication.java
@@ -0,0 +1,13 @@
+package com.veda.grampanchayat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrampanchayatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrampanchayatApplication.class, args);
+	}
+
+}
